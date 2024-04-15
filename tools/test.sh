0c71958ba74b1a8ea6f45a7ab54306ce9e0d12a7
Kari Pihkala <kari.pihkala@gmail.com>
2024-04-15 12:24:09 +0300

    Debug test.sh

--- tools/test.sh
+++ tools/test.sh
@@ -8,19 +8,13 @@ cargo clippy -- -D warnings
 cargo doc
 
 # run all tests
-cargo test
+cargo test --tests
 
 echo "---t1"
 ls -la
 echo "---t2"
 ls -la toisto-au-test-suite
 echo "---t3"
-ls -la ../target
-echo "---t4"
-ls -la ../target/debug
-echo "---t5"
-ls -la ../target/debug/examples
-echo "---t6"
 
 # build the toisto tester
 cargo build --example ausnd-aureader-toisto
@@ -29,6 +23,15 @@ cargo build --example ausnd-aureader-toisto
 echo
 echo 'Toisto AU test suite results:'
 cd toisto-au-test-suite
+
+echo "---t13"
+ls -la ../target
+echo "---t14"
+ls -la ../target/debug
+echo "---t15"
+ls -la ../target/debug/examples
+echo "---t16"
+
 python3 toisto-runner.py -c -v --override-list ../toisto-ausnd-override-list.json ../target/debug/examples/ausnd-aureader-toisto
 
 echo
