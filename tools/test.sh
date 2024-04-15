b653feaea250bb2c47cf56a7f54c9e6f435ff92c
Kari Pihkala <kari.pihkala@gmail.com>
2024-04-15 13:13:03 +0300

    Fix path in test.sh

--- tools/test.sh
+++ tools/test.sh
@@ -7,6 +7,10 @@ cargo clippy -- -D warnings
 # ensure documentation can be built
 cargo doc
 
+echo "---t1"
+ls -laR target
+echo "---t2"
+
 # run all tests
 cargo test --tests
 
@@ -18,20 +22,20 @@ echo
 echo 'Toisto AU test suite results:'
 cd toisto-au-test-suite
 
-echo "---t1"
+echo "---t3"
 ls -laR ../target
-echo "---t2"
+echo "---t4"
 
 # target/debug for normal testing,
 # x86_64-unknown-linux-gnu and powerpc-unknown-linux-gnu for GitHub Actions
-if [ -e ../target/x86_64-unknown-linux-gnu/examples/ausnd-aureader-toisto ]
+if [ -e ../target/x86_64-unknown-linux-gnu/debug/examples/ausnd-aureader-toisto ]
 then
     echo "TARGET: x86_64-unknown-linux-gnu"
-    python3 toisto-runner.py -c -v --override-list ../toisto-ausnd-override-list.json ../target/x86_64-unknown-linux-gnu/examples/ausnd-aureader-toisto
-elif [ -e ../target/powerpc-unknown-linux-gnu/examples/ausnd-aureader-toisto ]
+    python3 toisto-runner.py -c -v --override-list ../toisto-ausnd-override-list.json ../target/x86_64-unknown-linux-gnu/debug/examples/ausnd-aureader-toisto
+elif [ -e ../target/powerpc-unknown-linux-gnu/debug/examples/ausnd-aureader-toisto ]
 then
     echo "TARGET: powerpc-unknown-linux-gnu"
-    python3 toisto-runner.py -c -v --override-list ../toisto-ausnd-override-list.json ../target/powerpc-unknown-linux-gnu/examples/ausnd-aureader-toisto
+    python3 toisto-runner.py -c -v --override-list ../toisto-ausnd-override-list.json ../target/powerpc-unknown-linux-gnu/debug/examples/ausnd-aureader-toisto
 else
     python3 toisto-runner.py -c --override-list ../toisto-ausnd-override-list.json ../target/debug/examples/ausnd-aureader-toisto
 fi
